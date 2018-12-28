079c5ebc52ea6737ed90b4af7784ce0d132ee65d
unknown <mkavo92@gmail.com>
2018-12-27 20:44:20 -0600

    todo list progress

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { Todo } from "src/app/models/todo.model";
+import { Observable } from "rxjs";
+import { TodoService } from "src/app/service/todo.service";
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  selector: "app-todo-list",
+  templateUrl: "./todo-list.component.html",
+  styleUrls: ["./todo-list.component.scss"]
 })
 export class TodoListComponent implements OnInit {
+  todos$: Observable<Todo[]>;
 
-  constructor() { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {
+    this.todos$ = this.todoService.todos$;
   }
 
+  onChangeTodoStatus(todo: Todo) {
+    this.todoService.changeTodoStatus(todo.id, todo.isCompleted);
+  }
 }
--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,29 +1,32 @@
-import { Injectable } from '@angular/core';
-import { Todo } from '../models/todo.model';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Filter } from '../models/filtering.model';
-import { LocalStorageService } from './local-storage.service';
+import { Injectable } from "@angular/core";
+import { Todo } from "../models/todo.model";
+import { BehaviorSubject, Observable } from "rxjs";
+import { Filter } from "../models/filtering.model";
+import { LocalStorageService } from "./local-storage.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TodoService {
-// STORAGE key la2m thanh 1 bien de ko phai viet todo de tuong tac vs localstorage
+  // STORAGE key la2m thanh 1 bien de ko phai viet todo de tuong tac vs localstorage
 
-//
-  private static readonly TodoStorageKey = 'todos';
-// todos là tổng todo mình có đc
+  //
+  private static readonly TodoStorageKey = "todos";
+  // todos là tổng todo mình có đc
   private todos: Todo[];
   // filteredTodos là tods mình sẽ thấy trên front-end của mình
   private filteredTodos: Todo[];
   // lenght sẽ theo dõi length của todos
   // behaviorsubject sẽ phải nhận vô giá trị ban đầu, giá trị ban đầu là 0
-  private lengthSubject: BehaviorSubject<number> =  new BehaviorSubject<number>(0);
+  private lengthSubject: BehaviorSubject<number> = new BehaviorSubject<number>(
+    0
+  );
 
   // displayTodoSubject sẽ theo dõi filteredTodo
   // sẽ bắt đầu = 1 empty array
-  private displayTodoSubject: BehaviorSubject<Todo[]> = new BehaviorSubject<Todo[]>([]);
-
+  private displayTodoSubject: BehaviorSubject<Todo[]> = new BehaviorSubject<
+    Todo[]
+  >([]);
 
   private currentFilter: Filter = Filter.All;
 
@@ -32,61 +35,55 @@ export class TodoService {
   todos$: Observable<Todo[]> = this.displayTodoSubject.asObservable();
   length$: Observable<number> = this.lengthSubject.asObservable();
 
+  constructor(private storageService: LocalStorageService) {}
 
-  constructor(private storageService: LocalStorageService) { }
-
-
-  fetchFromLocalStorage(){
+  fetchFromLocalStorage() {
     // dùng hàm getValue để truyền vào type là  <Todo[]> sau đó chuyền qua key
     // nếu trong localstorage có todos thì trả về giá trị Todo array và gán giá trị vào mảng todos
     // nếu ko có gì thì trả về empty array
-    this.todos = this.storageService.getValue<Todo[]>(TodoService.TodoStorageKey) || [];
+    this.todos =
+      this.storageService.getValue<Todo[]>(TodoService.TodoStorageKey) || [];
 
     // filteredTodos là 1 mảng
     // shallow clone
     // deep clone mình copy phần tử ở trong cái todos
     // có thể sử dụng cloneDeep của lodash
     //
-    this.filteredTodos = [...this.todos.map(todo => ({...todo}))];
+    this.filteredTodos = [...this.todos.map(todo => ({ ...todo }))];
     this.updateTodosData();
-
   }
 
-
-// khi làm add, edit, delete todo thì mình sẽ thay đổi thằng this.todos
-  updateToLocalStorage(){
+  // khi làm add, edit, delete todo thì mình sẽ thay đổi thằng this.todos
+  updateToLocalStorage() {
     //
     this.storageService.setObject(TodoService.TodoStorageKey, this.todos);
     //
     this.filterTodos(this.currentFilter, false);
-    this.updateTodosData()
+    this.updateTodosData();
   }
 
-
   //
-   filterTodos(filter: Filter, isFiltering: boolean = true){
-
+  filterTodos(filter: Filter, isFiltering: boolean = true) {
     // gán lại thằng current filter
 
     this.currentFilter = filter;
-    switch(filter){
+    switch (filter) {
       case Filter.Active:
-      this.filteredTodos = this.todos.filter(todo => !todo.isCompleted);
-      break;
+        this.filteredTodos = this.todos.filter(todo => !todo.isCompleted);
+        break;
       case Filter.Completed:
-      this.filteredTodos = this.todos.filter(todo => todo.isCompleted);
-      break;
+        this.filteredTodos = this.todos.filter(todo => todo.isCompleted);
+        break;
       case Filter.All:
-      this.filteredTodos = [...this.todos.filter(todo => ({...todo}))];
-      break;
+        this.filteredTodos = [...this.todos.filter(todo => ({ ...todo }))];
+        break;
     }
 
     //
-     if(isFiltering){
-       this.updateTodosData();
-     }
-   }
-
+    if (isFiltering) {
+      this.updateTodosData();
+    }
+  }
 
   private updateTodosData() {
     this.displayTodoSubject.next(this.filteredTodos);
@@ -94,7 +91,7 @@ export class TodoService {
   }
 
   // addTodo nhận vào content là string
-  addTodo(content: string){
+  addTodo(content: string) {
     // xài getTime để trả về miliseconds
     const date = new Date(Date.now()).getTime(); //will be a number
 
@@ -103,6 +100,14 @@ export class TodoService {
 
     //unshift thì khi add nó sẽ hiện ở đầu
     this.todos.unshift(newTodo);
+    this.updateToLocalStorage();
+  }
+
+  changeTodoStatus(id: number, isCompleted: boolean) {
+    const index = this.todos.findIndex(t => t.id === id);
+    const todo = this.todos[index];
+    todo.isCompleted = isCompleted;
+    this.todos.splice(index, 1, todo);
     this.updateToLocalStorage();
   }
 }
